e87d7f7ec503834648062126dae351abbfbf0ecb
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2021-10-06 22:19:15 -0300

    Don't call sudo inside scripts, tell the user to run as root instead

--- uninstall.sh
+++ uninstall.sh
@@ -8,12 +8,12 @@ SERVICE_NAME=snapdSB.service
 SERVICE_UNINSTALL=snapdSB_uninstall.service
 
 # remove service
-sudo rm -rf ${SERVICE_FOLDER}${SERVICE_NAME}
+rm -rf ${SERVICE_FOLDER}${SERVICE_NAME}
 # uninstall script
-sudo rm -rf ${SCRIPT_FOLDER}
+rm -rf ${SCRIPT_FOLDER}
 
 # create service to remove /snap and return to symlink to /home
 # symlink to /home is not working
-sudo cp -f ${SERVICE_UNINSTALL} ${SERVICE_FOLDER}
-sudo ln -sf ${SERVICE_FOLDER}${SERVICE_UNINSTALL} \
+cp -f ${SERVICE_UNINSTALL} ${SERVICE_FOLDER}
+ln -sf ${SERVICE_FOLDER}${SERVICE_UNINSTALL} \
 	${SERVICE_FOLDER}remote-fs.target.wants/${SERVICE_UNINSTALL}
